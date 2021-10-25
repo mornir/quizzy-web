3fc90723e6358ca223bd1fcafdee83723a1910b7
mornir <jeromepott@hotmail.com>
2021-10-25 07:06:55 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,37 @@
 <template>
   <main class="p-8">
     <form>
-      <h1 class="text-5xl mb-4">Quiz</h1>
-      <RadioGroup v-model="plan" class="cursor-pointer group">
+      <h1 class="mb-4 text-5xl">Quiz</h1>
+      <RadioGroup v-model="plan" class="cursor-pointer">
         <RadioGroupLabel
           >Auf welche Unterlage soll ich Kerzen stellen?</RadioGroupLabel
         >
         <RadioGroupOption v-slot="{ checked }" value="startup">
-          <div class="flex items-center gap-x-4">
+          <div class="flex items-center gap-x-4 group">
             <div
-              class="
-                h-3
-                w-3
-                rounded-full
-                ring-2 ring-yellow-500 ring-offset-2
-                group-hover:ring-red-500
-                transition-colors
-                duration-500
-                ease-in-out
-              "
+              class="w-3 h-3 transition-all duration-500 ease-in rounded-full  ring-2 ring-yellow-500 ring-offset-2 group-hover:ring-red-500"
               :class="{ 'bg-red-500': checked }"
             ></div>
             <span>Startup</span>
           </div>
         </RadioGroupOption>
         <RadioGroupOption v-slot="{ checked }" value="business">
-          <span>Business</span>
+          <div class="flex items-center gap-x-4 group">
+            <div
+              class="w-3 h-3 transition-all duration-500 ease-in rounded-full  ring-2 ring-yellow-500 ring-offset-2 group-hover:ring-red-500"
+              :class="{ 'bg-red-500': checked }"
+            ></div>
+            <span>Business</span>
+          </div>
         </RadioGroupOption>
         <RadioGroupOption v-slot="{ checked }" value="enterprise">
-          <span>Enterprise</span>
+          <div class="flex items-center gap-x-4 group">
+            <div
+              class="w-3 h-3 transition-all duration-500 ease-in rounded-full  ring-2 ring-yellow-500 ring-offset-2 group-hover:ring-red-500"
+              :class="{ 'bg-red-500': checked }"
+            ></div>
+            <span>Enterprise</span>
+          </div>
         </RadioGroupOption>
       </RadioGroup>
     </form>
---
+++ src/components/QuizOption.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex items-center gap-x-4">
+    <div
+      class="
+        h-3
+        w-3
+        rounded-full
+        ring-2 ring-yellow-500 ring-offset-2
+        group-hover:ring-red-500
+        transition-colors
+        duration-500
+        ease-in-out
+      "
+      :class="{ 'bg-red-500': checked }"
+    ></div>
+    <span>Startup</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+}
+</script>
