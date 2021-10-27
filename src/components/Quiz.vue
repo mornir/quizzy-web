8d1b57917433f5e9f6e381c58766e89e18b5ca32
mornir <jeromepott@hotmail.com>
2021-10-27 12:08:21 +0200

    keep quiz questions on same page in case somebody shares the URL

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -11,49 +11,71 @@
         {{ quiz.description }}
       </p>
 
-      <router-view
-        :questions="quiz.questions"
-        @selected="checkAnswer"
-      ></router-view>
-      <p class="mb-8 font-light text-primary" v-if="showWrongAnswerHint">
-        Sorry, falsche Antwort. Versuchen Sie’s noch einmal.
-      </p>
-      <div class="flex justify-center lg:justify-start">
-        <button
-          @click="next"
-          type="button"
-          :disabled="isDisabled"
-          class="px-8 py-2 text-lg font-bold text-white uppercase rounded-full bg-primary disabled:opacity-25"
-        >
-          Weiter
-        </button>
-      </div>
+      <form @submit.prevent="checkAnswer">
+        <RadioGroup v-model="answer" class="mb-8">
+          <RadioGroupLabel>
+            <div class="mb-6 font-bold">
+              <span class="uppercase text-primary"
+                >Frage {{ step }}: <br />
+              </span>
+              <span class="text-secondary">{{ question.title }}</span>
+            </div>
+          </RadioGroupLabel>
+          <RadioGroupOption
+            v-slot="{ checked }"
+            :value="choice._key"
+            v-for="choice in question?.choices"
+            :key="choice._key"
+          >
+            <div class="flex items-center mb-3 cursor-pointer gap-x-4 group">
+              <div
+                class="flex-shrink-0 w-3 h-3 transition-all duration-500 ease-in rounded-full ring-2 ring-yellow-500 ring-offset-2 group-hover:ring-red-500"
+                :class="{ 'bg-red-500': checked }"
+              ></div>
+              <span class="font-light">{{ choice.title }}</span>
+            </div>
+          </RadioGroupOption>
+        </RadioGroup>
+
+        <p class="mb-8 font-light text-primary" v-if="showWrongAnswerHint">
+          Sorry, falsche Antwort. Versuchen Sie’s noch einmal.
+        </p>
+        <div class="flex justify-center lg:justify-start">
+          <button
+            @click="next"
+            :disabled="!answer"
+            class="px-8 py-2 text-lg font-bold text-white uppercase rounded-full bg-primary disabled:opacity-25"
+          >
+            Weiter
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
 import QuizForm from './QuizForm.vue'
+import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
 import sanity from '../sanity'
 export default {
   name: 'Quiz',
   components: {
     QuizForm,
+    RadioGroup,
+    RadioGroupLabel,
+    RadioGroupOption,
   },
   data() {
     return {
       quiz: {},
       step: 1,
-      isDisabled: true,
-      isAnswerCorrect: false,
       showWrongAnswerHint: false,
       showForm: false,
+      answer: '',
     }
   },
   created() {
-    if (this.$route.params.step) {
-      this.step = this.$route.params.step
-    }
     sanity
       .fetch('*[_type == "quiz"  && slug.current == $slug][0]', {
         slug: this.$route.params.slug,
@@ -61,19 +83,33 @@ export default {
       .then((quiz) => (this.quiz = quiz))
       .catch((err) => console.error('Oh noes: %s', err.message))
   },
-  methods: {
-    checkAnswer(answer) {
-      this.showWrongAnswerHint = false
-      this.isDisabled = false
-      this.isAnswerCorrect = answer
+  computed: {
+    question() {
+      const question = this.quiz?.questions?.[this.step - 1]
+      if (question) {
+        return question
+      } else {
+        return {}
+      }
     },
-    next() {
-      if (this.isAnswerCorrect) {
+  },
+  watch: {
+    answer() {
+      this.showWrongAnswerHint = false
+    },
+  },
+  methods: {
+    checkAnswer() {
+      const isAnswerCorrect = this.question.choices.find(
+        (choice) => choice._key === this.answer
+      ).isCorrect
+
+      if (isAnswerCorrect) {
         if (this.step >= this.quiz.questions.length) {
           this.showForm = true
-          //this.$router.push({ params: { step: this.step } })
         } else {
           this.step++
+          this.answer = ''
           this.$router.push({ params: { step: this.step } })
         }
       } else {
--- src/router.js
+++ src/router.js
@@ -1,36 +1,18 @@
-// 1. Define route components.
-// These can be imported from other files
 import { createRouter, createWebHistory } from 'vue-router'
 import QuizList from './components/QuizList.vue'
 import Quiz from './components/Quiz.vue'
-import QuizQuestion from './components/QuizQuestion.vue'
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 const routes = [
   { path: '/', component: QuizList },
   { path: '/quiz/:slug', component: Quiz },
   {
     path: '/quiz/:slug/:step',
     component: Quiz,
-    children: [
-      // QuizQuestion will be rendered inside Quiz's <router-view>
-      // when /quiz/:slug/:step is matched
-      { path: '', component: QuizQuestion },
-
-      // ...other sub routes
-    ],
   },
 ]
-
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHistory(),
-  routes, // short for `routes: routes`
+  routes,
 })
 
 export default router
